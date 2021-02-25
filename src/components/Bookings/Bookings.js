2a14ded1b6497e90f747af1f4d83bc40e7b3d686
mdabdullaalnoman <noman.apparel.4003@gmail.com>
2021-02-25 23:30:48 +0600

    doke

--- src/components/Bookings/Bookings.js
+++ src/components/Bookings/Bookings.js
@@ -3,28 +3,32 @@ import { UserContext } from '../../App';
 
 const Bookings = () => {
     const [loggedInUser, setLoggedInUser] = useContext(UserContext);
-    const [booking , setBooking] = useState([]);
+    const [booking, setBooking] = useState([]);
 
-    useEffect(() =>{
-        fetch('http://localhost:4200/bookings?email='+loggedInUser.email,{
-            method:'GET',
-            headers:{
-                'Content-Type' :'application/json',
-                authorization : `Bearer ${sessionStorage.getItem('token')}`
+    useEffect(() => {
+        fetch('http://localhost:4200/bookings?email=' + loggedInUser.email, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                authorization: `Bearer ${sessionStorage.getItem('token')}`
             }
         })//quarry perametter to load only loggedin user info .
-        .then(res => res.json())
-        .then(data => setBooking(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setBooking(data))
+    }, [])
     return (
-        <div style={{textAlign:'center'}}>
+        <div style={{ textAlign: 'center' }}>
             <h1> I Have {booking.length} bookings</h1>
             {
                 booking.map(bookInfo => (
-                    <div style={{textAlign:'center', padding:'10px' , color:'white', background:'black', margin:'10px'}}>
-                        <h3>{bookInfo.name}</h3>
-                        <h2>{bookInfo.email} </h2>
-                        <p>{(new Date(bookInfo.checkInDate).toDateString('dd/MM/yyyy'))}</p>
+                    <div className="container">
+                        <div className="row text-center">
+                            <div className="col-6 px-2 my-2" style={{border: '1px solid gray'}}>
+                                <h4>{bookInfo.name}</h4>
+                                <h6>{bookInfo.email} </h6>
+                                <p>{(new Date(bookInfo.checkInDate).toDateString('dd/MM/yyyy'))}</p>
+                            </div>
+                        </div>
                     </div>
                 ))
             }
